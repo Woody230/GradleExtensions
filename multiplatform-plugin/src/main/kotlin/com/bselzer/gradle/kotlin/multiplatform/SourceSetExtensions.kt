6138eb7408b65842c435935580f8ca082af1a42a
Woody230 <bselzer1@outlook.com>
2023-05-08 08:04:13 -0400

    kotlin multiplatform dependencies

--- multiplatform-plugin/src/main/kotlin/com/bselzer/gradle/kotlin/multiplatform/SourceSetExtensions.kt
+++ multiplatform-plugin/src/main/kotlin/com/bselzer/gradle/kotlin/multiplatform/SourceSetExtensions.kt
@@ -1,82 +1,45 @@
 package com.bselzer.gradle.kotlin.multiplatform
 
 import org.gradle.api.NamedDomainObjectContainer
-import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 
-/**
- * Sets up common dependencies.
- */
-fun NamedDomainObjectContainer<KotlinSourceSet>.commonMain(block: KotlinDependencyHandler.() -> Unit = {}) {
-    getByName("commonMain") {
-        it.dependencies {
-            block(this)
-        }
-    }
-}
+val NamedDomainObjectContainer<KotlinSourceSet>.commonMain: KotlinSourceSet
+    get() = getByName("commonMain")
 
-/**
- * Sets up common test dependencies.
- */
-fun NamedDomainObjectContainer<KotlinSourceSet>.commonTest(block: KotlinDependencyHandler.() -> Unit = {}) {
-    getByName("commonTest") {
-        it.dependencies {
-            block(this)
-        }
-    }
-}
+fun NamedDomainObjectContainer<KotlinSourceSet>.commonMain(configure: KotlinSourceSet.() -> Unit) = commonMain.apply(configure)
 
-/**
- * Sets up JVM dependencies.
- */
-fun NamedDomainObjectContainer<KotlinSourceSet>.jvmMain(block: KotlinDependencyHandler.() -> Unit = {}) {
-    getByName("jvmMain") {
-        it.dependencies {
-            block(this)
-        }
-    }
-}
 
-/**
- * Sets up JVM test dependencies.
- */
-fun NamedDomainObjectContainer<KotlinSourceSet>.jvmTest(block: KotlinDependencyHandler.() -> Unit = {}) {
-    getByName("jvmTest") {
-        it.dependencies {
-            block(this)
-        }
-    }
-}
+val NamedDomainObjectContainer<KotlinSourceSet>.commonTest: KotlinSourceSet
+    get() = getByName("commonTest")
 
-/**
- * Sets up Android dependencies.
- */
-fun NamedDomainObjectContainer<KotlinSourceSet>.androidMain(block: KotlinDependencyHandler.() -> Unit = {}) {
-    getByName("androidMain") {
-        it.dependencies {
-            block(this)
-        }
-    }
-}
+fun NamedDomainObjectContainer<KotlinSourceSet>.commonTest(configure: KotlinSourceSet.() -> Unit) = commonTest.apply(configure)
 
-/**
- * Sets up Android unit test dependencies.
- */
-fun NamedDomainObjectContainer<KotlinSourceSet>.androidUnitTest(block: KotlinDependencyHandler.() -> Unit = {}) {
-    getByName("androidUnitTest") {
-        it.dependencies {
-            block(this)
-        }
-    }
-}
 
-/**
- * Sets up Android instrumented test dependencies.
- */
-fun NamedDomainObjectContainer<KotlinSourceSet>.androidInstrumentedTest(block: KotlinDependencyHandler.() -> Unit = {}) {
-    getByName("androidInstrumentedTest") {
-        it.dependencies {
-            block(this)
-        }
-    }
-}
+val NamedDomainObjectContainer<KotlinSourceSet>.jvmMain: KotlinSourceSet
+    get() = getByName("jvmMain")
+
+fun NamedDomainObjectContainer<KotlinSourceSet>.jvmMain(configure: KotlinSourceSet.() -> Unit) = jvmMain.apply(configure)
+
+
+val NamedDomainObjectContainer<KotlinSourceSet>.jvmTest: KotlinSourceSet
+    get() = getByName("jvmTest")
+
+fun NamedDomainObjectContainer<KotlinSourceSet>.jvmTest(configure: KotlinSourceSet.() -> Unit) = jvmTest.apply(configure)
+
+
+val NamedDomainObjectContainer<KotlinSourceSet>.androidMain: KotlinSourceSet
+    get() = getByName("androidMain")
+
+fun NamedDomainObjectContainer<KotlinSourceSet>.androidMain(configure: KotlinSourceSet.() -> Unit) = androidMain.apply(configure)
+
+
+val NamedDomainObjectContainer<KotlinSourceSet>.androidUnitTest: KotlinSourceSet
+    get() = getByName("androidUnitTest")
+
+fun NamedDomainObjectContainer<KotlinSourceSet>.androidUnitTest(configure: KotlinSourceSet.() -> Unit) = androidUnitTest.apply(configure)
+
+
+val NamedDomainObjectContainer<KotlinSourceSet>.androidInstrumentedTest: KotlinSourceSet
+    get() = getByName("androidInstrumentedTest")
+
+fun NamedDomainObjectContainer<KotlinSourceSet>.androidInstrumentedTest(configure: KotlinSourceSet.() -> Unit) = androidInstrumentedTest.apply(configure)
