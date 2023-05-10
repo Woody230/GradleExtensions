53c88be73844a7cc98051717de3e77f365061ba7
Woody230 <bselzer1@outlook.com>
2023-05-09 22:34:24 -0400

    cleanup build.gradle

--- multiplatform-plugin/build.gradle.kts
+++ multiplatform-plugin/build.gradle.kts
@@ -3,23 +3,7 @@ plugins {
     alias(libs.plugins.jvm)
 }
 
-buildscript {
-    dependencies {
-        classpath(libs.moko.resources.generator)
-    }
-}
-
 dependencies {
-    api(projects.androidPlugin)
     api(projects.api)
     api(projects.multiplatform)
-    api(libs.compose.gradle.plugin)
-    api(libs.android.gradle.plugin)
-    api(libs.moko.resources.gradle.plugin)
-    api(libs.kotlin.gradle.plugin)
-    api(libs.vanniktech.publish.gradle.plugin)
-    api(libs.dokka.gradle.plugin)
-
-    // TODO can't access libs https://github.com/gradle/gradle/issues/15383
-    implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
 }
