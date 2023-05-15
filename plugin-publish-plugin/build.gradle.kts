a131dc7ef8d8ee3337f473ba6e6315ecd7b2a873
Woody230 <bselzer1@outlook.com>
2023-05-15 10:37:43 -0400

    remove gradle plugin portal publish

--- plugin-publish-plugin/build.gradle.kts
+++ plugin-publish-plugin/build.gradle.kts
@@ -11,17 +11,3 @@ dependencies {
     api(libs.dokka.gradle.plugin)
     implementation(projects.mavenPublishPlugin)
 }
-
-gradlePlugin {
-    website.set("https://github.com/Woody230/KotlinExtensions")
-    vcsUrl.set("https://github.com/Woody230/KotlinExtensions.git")
-
-    plugins {
-        create("publishPlugin") {
-            id = "io.github.woody230.gradle.plugin.publish"
-            displayName = "Plugin Publishing"
-            description = "Internal publishing for gradle plugins."
-            implementationClass = "com.bselzer.gradle.plugin.publish.plugin.PluginPublishPlugin"
-        }
-    }
-}
