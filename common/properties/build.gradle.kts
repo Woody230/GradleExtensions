8d9c60a155a05b59ed289ebcc49aeb76afda754c
Woody230 <bselzer1@outlook.com>
2023-05-17 10:24:41 -0400

    properties jvm publish

--- common/properties/build.gradle.kts
+++ common/properties/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.bselzer.gradle.jvm.publish.plugin.jvmPublishExtension
+import com.bselzer.gradle.maven.publish.plugin.Licensing
+
 plugins {
     `kotlin-dsl`
 }
@@ -5,3 +8,13 @@ plugins {
 dependencies {
     api(gradleApi())
 }
+
+jvmPublishExtension {
+    subGroupId.set("gradle")
+    version.set(rootProject.version.toString())
+    repository.set("https://github.com/Woody230/GradleExtensions")
+    description.set("Gradle property extensions.")
+    licensing.set(Licensing.APACHE_2_0)
+}
+
+apply(plugin = libs.plugins.woody230.gradle.jvm.publish.get().pluginId)
