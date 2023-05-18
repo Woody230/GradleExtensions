2c24324f556a364f2cf08b597902ca7327a737a2
Woody230 <bselzer1@outlook.com>
2023-05-18 11:11:27 -0400

    fix path resolution

--- internal-publish-plugins/settings-plugin/src/main/kotlin/com/bselzer/gradle/settings/plugin/VersionCatalogPlugin.kt
+++ internal-publish-plugins/settings-plugin/src/main/kotlin/com/bselzer/gradle/settings/plugin/VersionCatalogPlugin.kt
@@ -13,6 +13,7 @@ class VersionCatalogPlugin : Plugin<Settings> {
     }
 
     override fun apply(settings: Settings) = with(settings) {
+        val toml = toml ?: return@with
         dependencyResolutionManagement {
             versionCatalogs {
                 create("libs") {
@@ -22,21 +23,30 @@ class VersionCatalogPlugin : Plugin<Settings> {
         }
     }
 
-    private val Settings.toml: File
+    private val Settings.toml: File?
         get() {
-            var path: Path? = Paths.get(rootDir.path)
+            var path: Path? = rootDir.toPath()
             while (true) {
                 if (path == null) {
                     throw FileNotFoundException("Expected to find the version catalog at $RELATIVE_PATH in the project root directory $rootDir or higher level directory but it does not exist.")
                 }
 
                 val file = Paths.get(path.toString(), RELATIVE_PATH).toFile()
-                if (file.exists()) {
-                    return file
+                if (!file.exists()) {
+                    // For composite builds we need to work backwards to the root of the composite.
+                    path = path.parent
+                    continue
                 }
 
-                // For composite builds we need to work backwards to the root of the composite.
-                path = path.parent
+                if (path == rootDir.toPath()) {
+                    // If applied from the root of the composite, then we should not add the file because it will be done by default.
+                    // https://github.com/gradle/gradle/issues/21328
+                    // https://github.com/gradle/gradle/issues/20282
+                    // https://github.com/gradle/gradle/blob/393a2ea92a43f04b75c367f0759266be9eff46ae/subprojects/core/src/main/java/org/gradle/configuration/BuildTreePreparingProjectsPreparer.java#L77-L86
+                    return null
+                }
+
+                return file
             }
         }
 }
