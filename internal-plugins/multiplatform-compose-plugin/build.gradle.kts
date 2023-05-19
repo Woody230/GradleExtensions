231da027f1a8fd6b6bc4120e6b3ec12370c2cfe9
Woody230 <bselzer1@outlook.com>
2023-05-19 19:06:04 -0400

    compose plugin => Android application or library configuring

--- internal-plugins/multiplatform-compose-plugin/build.gradle.kts
+++ internal-plugins/multiplatform-compose-plugin/build.gradle.kts
@@ -1,8 +1,8 @@
 import com.bselzer.gradle.plugin.publish.plugin.pluginPublishExtension
 
 dependencies {
-    api(libs.android.plugin)
     api(libs.compose.plugin)
+    implementation(libs.woody230.gradle.android)
 
     // TODO can't access libs https://github.com/gradle/gradle/issues/15383
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
