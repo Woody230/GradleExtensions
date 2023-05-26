105508437001ee424fd947c41c443297ec07fab3
Woody230 <bselzer1@outlook.com>
2023-05-26 07:45:16 -0400

    don't add assets to srcDir

--- internal-plugins/moko-resources-plugin/src/main/kotlin/com/bselzer/gradle/internal/moko/resources/plugin/MokoResourcesPlugin.kt
+++ internal-plugins/moko-resources-plugin/src/main/kotlin/com/bselzer/gradle/internal/moko/resources/plugin/MokoResourcesPlugin.kt
@@ -1,6 +1,6 @@
 package com.bselzer.gradle.internal.moko.resources.plugin
 
-import com.bselzer.gradle.android.androidExtension
+import com.bselzer.gradle.android.androidExtensionOrNull
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import java.io.File
@@ -11,12 +11,11 @@ class MokoResourcesPlugin : Plugin<Project> {
         plugins.apply("dev.icerock.mobile.multiplatform-resources")
 
         // TODO temporary srcDir inclusion https://github.com/icerockdev/moko-resources/issues/353
-        with(androidExtension.sourceSets.getByName("main")) {
-            assets.srcDir(File(buildDir, "generated/moko/androidMain/assets"))
-            res.srcDir(File(buildDir, "generated/moko/androidMain/res"))
+        androidExtensionOrNull?.sourceSets?.getByName("main")?.let { sourceSet ->
+            //sourceSet.assets.srcDir(File(buildDir, "generated/moko/androidMain/assets"))
+            sourceSet.res.srcDir(File(buildDir, "generated/moko/androidMain/res"))
         }
 
-        // TODO temporarily explicitly declare dependency
         tasks.whenTaskAdded {
             if (name == "generateMRandroidMain") {
                 tasks.withType(org.gradle.jvm.tasks.Jar::class.java).forEach { task -> task.dependsOn(this) }
@@ -25,7 +24,6 @@ class MokoResourcesPlugin : Plugin<Project> {
 
         tasks.withType(org.jetbrains.dokka.gradle.DokkaTask::class.java).forEach { task ->
             task.dependsOn("generateMRandroidMain")
-            task.dependsOn("generateMRjvmMain")
         }
     }
 }
