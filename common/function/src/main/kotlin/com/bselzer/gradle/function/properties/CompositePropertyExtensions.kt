7efeb7af3566e209eedb84e99369180d34c2768a
Woody230 <bselzer1@outlook.com>
2023-06-10 08:27:56 -0400

    add docs

--- common/function/src/main/kotlin/com/bselzer/gradle/function/properties/CompositePropertyExtensions.kt
+++ common/function/src/main/kotlin/com/bselzer/gradle/function/properties/CompositePropertyExtensions.kt
@@ -5,14 +5,26 @@ import org.gradle.api.Project
 import java.io.File
 import java.util.*
 
+/**
+ * The composite.properties from the root directory.
+ */
 val Project.compositeProperties: Properties
     get() = compositePropertiesFileOrNull.load()
 
+/**
+ * The composite.properties file from the root directory.
+ */
 val Project.compositePropertiesFileOrNull: File?
     get() = propertiesFileOrNull("composite.properties")
 
+/**
+ * The composite.properties from the root directory of this project or from a parent build.
+ */
 val Project.compositeCompositeProperties: Properties
     get() = compositeCompositePropertiesFileOrNull.load()
 
+/**
+ * The composite.properties file from the root directory of this project or from a parent build.
+ */
 val Project.compositeCompositePropertiesFileOrNull: File?
     get() = compositeBuildSequence().firstNotNullOfOrNull { build -> build.rootProject.compositePropertiesFileOrNull }
--- common/function/src/main/kotlin/com/bselzer/gradle/function/properties/LocalPropertyExtensions.kt
+++ common/function/src/main/kotlin/com/bselzer/gradle/function/properties/LocalPropertyExtensions.kt
@@ -5,14 +5,26 @@ import org.gradle.api.Project
 import java.io.File
 import java.util.*
 
+/**
+ * The local.properties from the root directory.
+ */
 val Project.localProperties: Properties
     get() = localPropertiesFileOrNull.load()
 
+/**
+ * The local.properties file from the root directory.
+ */
 val Project.localPropertiesFileOrNull: File?
     get() = propertiesFileOrNull("local.properties")
 
+/**
+ * The local.properties from the root directory of this project or from a parent build.
+ */
 val Project.compositeLocalProperties: Properties
     get() = compositeLocalPropertiesFileOrNull.load()
 
+/**
+ * The local.properties file from the root directory of this project or from a parent build.
+ */
 val Project.compositeLocalPropertiesFileOrNull: File?
     get() = compositeBuildSequence().firstNotNullOfOrNull { build -> build.rootProject.localPropertiesFileOrNull }
