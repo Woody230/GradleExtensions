4708c369cd3eb72fd4b9c8700c874252104a1b79
Woody230 <bselzer1@outlook.com>
2023-06-11 10:36:55 -0400

    composite publish => add description + group to tasks

--- internal-settings-plugins/composite-publish-plugin/src/main/kotlin/com/bselzer/gradle/internal/composite/publish/plugin/CompositePublishPlugin.kt
+++ internal-settings-plugins/composite-publish-plugin/src/main/kotlin/com/bselzer/gradle/internal/composite/publish/plugin/CompositePublishPlugin.kt
@@ -5,6 +5,10 @@ import org.gradle.api.Project
 import org.gradle.api.initialization.Settings
 
 class CompositePublishPlugin : Plugin<Settings> {
+    private companion object {
+        const val PUBLISH_GROUP = "publishing"
+    }
+
     override fun apply(settings: Settings) = with(settings) {
         gradle.projectsLoaded {
             if (parent == null) {
@@ -18,11 +22,17 @@ class CompositePublishPlugin : Plugin<Settings> {
     private fun Project.addRootTasks() {
         // NOTE assumption is that the root project does not actually have any projects itself and purely relies on included builds.
         tasks.register("publishBuildsToMavenCentralRepository") {
+            group = PUBLISH_GROUP
+            description = "Publishes all projects within the included builds to the Maven Central repository."
+
             val tasks = gradle.includedBuilds.map { build -> build.task(":publishBuildToMavenCentralRepository") }
             dependsOn(tasks)
         }
 
         tasks.register("publishBuildsToMavenLocal") {
+            group = PUBLISH_GROUP
+            description = "Publishes all projects within the included builds to the Maven local repository."
+
             val tasks = gradle.includedBuilds.map { build -> build.task(":publishBuildToMavenLocal") }
             dependsOn(tasks)
         }
@@ -30,11 +40,17 @@ class CompositePublishPlugin : Plugin<Settings> {
 
     private fun Project.addLeafTasks() {
         tasks.register("publishBuildToMavenCentralRepository") {
+            group = PUBLISH_GROUP
+            description = "Publishes all projects within this build to the Maven Central repository."
+
             val tasks = getTasksByName("publishAllPublicationsToMavenCentralRepository", true)
             dependsOn(tasks)
         }
 
         tasks.register("publishBuildToMavenLocal") {
+            group = PUBLISH_GROUP
+            description = "Publishes all projects within this build to the Maven local repository."
+
             val tasks = getTasksByName("publishToMavenLocal", true)
             dependsOn(tasks)
         }
