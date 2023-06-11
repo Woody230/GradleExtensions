e482f81f2149222665de355534d9b79b793006f9
Woody230 <bselzer1@outlook.com>
2023-06-10 21:12:59 -0400

    explicit set root project name

--- internal-settings-plugins/bundled-plugin/src/main/kotlin/com/bselzer/gradle/internal/bundled/plugin/BundledPlugin.kt
+++ internal-settings-plugins/bundled-plugin/src/main/kotlin/com/bselzer/gradle/internal/bundled/plugin/BundledPlugin.kt
@@ -19,10 +19,8 @@ class BundledPlugin : Plugin<Settings> {
             }
         }
 
-        // TODO feature preview https://docs.gradle.org/8.1.1/userguide/declaring_dependencies.html#sec:type-safe-project-accessors
-        enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
-        plugins.apply(FoojayToolchainsConventionPlugin::class.java)
+        enableTypeSafeProjectAccessors()
+        applyToolchainPlugin()
         applyInternalPlugins()
     }
 
@@ -36,5 +34,18 @@ class BundledPlugin : Plugin<Settings> {
     private fun Settings.applyInternalPlugins() {
         plugins.apply(CompositePropertyPlugin::class.java)
         plugins.apply(VersionCatalogPlugin::class.java)
+    }
+
+    private fun Settings.applyToolchainPlugin() {
+        plugins.apply(FoojayToolchainsConventionPlugin::class.java)
+    }
+
+    private fun Settings.enableTypeSafeProjectAccessors() {
+        // TODO feature preview https://docs.gradle.org/8.1.1/userguide/declaring_dependencies.html#sec:type-safe-project-accessors
+        enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+        // NOTE: need to ensure the root project name is set to resolve the following warning
+        // Project accessors enabled, but root project name not explicitly set for '$NAME'. Checking out the project in different folders will impact the generated code and implicitly the buildscript classpath, breaking caching.
+        rootProject.name = rootProject.projectDir.name
     }
 }
