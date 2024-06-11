8345b293278fbeb7356189bbc1925f387d988522
Woody230 <bselzer1@outlook.com>
2024-06-11 17:00:25 -0400

    update compose version

--- internal-project-plugins/multiplatform-compose-plugin/src/main/kotlin/com/bselzer/gradle/internal/multiplatform/compose/plugin/MultiplatformComposePlugin.kt
+++ internal-project-plugins/multiplatform-compose-plugin/src/main/kotlin/com/bselzer/gradle/internal/multiplatform/compose/plugin/MultiplatformComposePlugin.kt
@@ -5,15 +5,10 @@ import com.bselzer.gradle.android.androidComponentsExtensionOrNull
 import com.bselzer.gradle.android.finalizeDslReceiver
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.getByType
-import org.jetbrains.compose.ComposeExtension
 
 class MultiplatformComposePlugin : Plugin<Project> {
     override fun apply(project: Project): Unit = with(project) {
-        val extension = multiplatformComposeExtension {
-            // TODO libs.versions.androidx.compose.compiler.get()
-            compilerVersion.convention("1.5.10")
-        }
+        val extension = multiplatformComposeExtension
 
         // NOTE: Must configure in finalizeDsl not afterEvaluate
         // https://developer.android.com/build/extend-agp#build-flow-extension-points
@@ -23,10 +18,6 @@ class MultiplatformComposePlugin : Plugin<Project> {
 
         // TODO libs.plugins.compose.get().pluginId
         plugins.apply("org.jetbrains.compose")
-
-        extensions.getByType<ComposeExtension>().apply {
-            configureCompose(extension)
-        }
     }
 
     private fun CommonExtension<*, *, *, *, *>.configureCompose(
@@ -39,7 +30,7 @@ class MultiplatformComposePlugin : Plugin<Project> {
             // https://www.jetbrains.com/help/kotlin-multiplatform-dev/compose-compatibility-and-versioning.html#kotlin-compatibility
             // https://github.com/JetBrains/compose-multiplatform/blob/master/gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeCompilerCompatibility.kt
             // https://developer.android.com/jetpack/androidx/releases/compose-kotlin#pre-release_kotlin_compatibility
-            kotlinCompilerExtensionVersion = extension.compilerVersion.get()
+            kotlinCompilerExtensionVersion = extension.compilerVersion.orNull
         }
 
         packaging {
@@ -48,12 +39,5 @@ class MultiplatformComposePlugin : Plugin<Project> {
                 add("META-INF/LGPL2.1")
             }
         }
-    }
-
-    private fun ComposeExtension.configureCompose(extension: MultiplatformComposeExtension) {
-        // https://www.jetbrains.com/help/kotlin-multiplatform-dev/compose-compatibility-and-versioning.html#jetpack-compose-and-compose-multiplatform-release-cycles
-        // NOTE: using the Jetpack compiler is fine when relying only on Android/Desktop targets, otherwise the multiplatform version should be used
-        // TODO libs.androidx.compose.compiler.get()
-        kotlinCompilerPlugin.set("androidx.compose.compiler:compiler:${extension.compilerVersion.get()}")
     }
 }
