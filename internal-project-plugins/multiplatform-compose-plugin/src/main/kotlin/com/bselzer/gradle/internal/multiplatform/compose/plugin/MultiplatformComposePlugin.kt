e6553fd3906744e91631da29d45e1e631d7fde63
Woody230 <bselzer1@outlook.com>
2024-06-18 07:36:17 -0400

    remove android compose compiler setting

--- internal-project-plugins/multiplatform-compose-plugin/src/main/kotlin/com/bselzer/gradle/internal/multiplatform/compose/plugin/MultiplatformComposePlugin.kt
+++ internal-project-plugins/multiplatform-compose-plugin/src/main/kotlin/com/bselzer/gradle/internal/multiplatform/compose/plugin/MultiplatformComposePlugin.kt
@@ -8,12 +8,10 @@ import org.gradle.api.Project
 
 class MultiplatformComposePlugin : Plugin<Project> {
     override fun apply(project: Project): Unit = with(project) {
-        val extension = multiplatformComposeExtension
-
         // NOTE: Must configure in finalizeDsl not afterEvaluate
         // https://developer.android.com/build/extend-agp#build-flow-extension-points
         androidComponentsExtensionOrNull?.finalizeDslReceiver {
-            configureCompose(extension)
+            configureCompose()
         }
 
         with (pluginManager) {
@@ -25,19 +23,10 @@ class MultiplatformComposePlugin : Plugin<Project> {
         }
     }
 
-    private fun CommonExtension<*, *, *, *, *, *>.configureCompose(
-        extension: MultiplatformComposeExtension
-    ) {
+    private fun CommonExtension<*, *, *, *, *, *>.configureCompose() {
         buildFeatures {
             compose = true
         }
-        composeOptions {
-            // https://www.jetbrains.com/help/kotlin-multiplatform-dev/compose-compatibility-and-versioning.html#kotlin-compatibility
-            // https://github.com/JetBrains/compose-multiplatform/blob/master/gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeCompilerCompatibility.kt
-            // https://developer.android.com/jetpack/androidx/releases/compose-kotlin#pre-release_kotlin_compatibility
-            kotlinCompilerExtensionVersion = extension.compilerVersion.orNull
-        }
-
         packaging {
             resources.pickFirsts.apply {
                 add("META-INF/AL2.0")
