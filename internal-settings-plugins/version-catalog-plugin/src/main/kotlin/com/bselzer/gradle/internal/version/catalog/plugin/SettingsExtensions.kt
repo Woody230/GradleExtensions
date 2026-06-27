47088bd76e08e5a4f0944a224128bb0bf60db405
Brandon Selzer <bselzer1@outlook.com>
2026-06-27 12:00:17 -0400

    avoid recreating libs

--- internal-settings-plugins/version-catalog-plugin/src/main/kotlin/com/bselzer/gradle/internal/version/catalog/plugin/SettingsExtensions.kt
+++ internal-settings-plugins/version-catalog-plugin/src/main/kotlin/com/bselzer/gradle/internal/version/catalog/plugin/SettingsExtensions.kt
@@ -2,11 +2,12 @@ package com.bselzer.gradle.internal.version.catalog.plugin
 
 import com.bselzer.gradle.function.settings.files
 import org.gradle.api.initialization.Settings
+import org.gradle.api.logging.Logger
 import java.io.File
 import java.nio.file.Path
 import java.nio.file.Paths
 
-internal fun Settings.createVersionCatalog(name: String, file: File) {
+internal fun Settings.createVersionCatalog(logger: Logger, name: String, file: File) {
     // NOTE the gradle/libs.versions.toml within the root directory is automatically included and must not be added again
     // https://github.com/gradle/gradle/issues/20282
     if (file.startsWith(rootDir)) {
@@ -15,7 +16,13 @@ internal fun Settings.createVersionCatalog(name: String, file: File) {
 
     dependencyResolutionManagement {
         versionCatalogs {
+            if (names.contains(name)) {
+                logger.info("Skipping creation of the $name version catalog because it already exists.")
+                return@versionCatalogs
+            }
+
             create(name) {
+                logger.info("Creating the $name version catalog from ${file.path}")
                 from(files(file))
             }
         }
--- internal-settings-plugins/version-catalog-plugin/src/main/kotlin/com/bselzer/gradle/internal/version/catalog/plugin/VersionCatalogPlugin.kt
+++ internal-settings-plugins/version-catalog-plugin/src/main/kotlin/com/bselzer/gradle/internal/version/catalog/plugin/VersionCatalogPlugin.kt
@@ -2,9 +2,16 @@ package com.bselzer.gradle.internal.version.catalog.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
+import org.gradle.api.logging.Logging
 
 class VersionCatalogPlugin : Plugin<Settings> {
     override fun apply(settings: Settings) = with(settings) {
-        createVersionCatalog(name = "libs", versionsFile)
+        val logger = Logging.getLogger(InternalVersionCatalogPlugin::class.java)
+
+        // NOTE: need to be able to avoid recreating libs after dev.panuszewski.typesafe-conventions
+        // https://github.com/radoslaw-panuszewski/typesafe-conventions-gradle-plugin/blob/main/src/main/kotlin/dev/panuszewski/gradle/conventioncatalogs/ConventionCatalogPlugin.kt
+        gradle.settingsEvaluated {
+            createVersionCatalog(logger, name = "libs", file = versionsFile)
+        }
     }
 }
